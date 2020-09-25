2285cf2e2f32d8f7c57d67e09e35ba5b8e54993d
Daywey <wmcompass@users.noreply.github.com>
2020-09-25 08:48:01 +0800

    20200925

--- display/models.py
+++ display/models.py
@@ -5,12 +5,12 @@ from django.db import models
 
 class purchase(models.Model):
     orderDate = models.DateField()
-    orderNumber = models.CharField(max_length=100)
-    username = models.CharField(max_length=250, blank = True)
+    orderNumber = models.CharField(max_length=100, blank = False, unique = True)
+    username = models.CharField(max_length=250, blank = False)
     customerDesc = models.CharField(max_length=250)
-    orderDesc = models.CharField(max_length=500, blank=True)
-    amount = models.FloatField()
-    group = models.CharField(max_length=250)
+    orderDesc = models.CharField(max_length=500)
+    amount = models.FloatField(blank = False)
+    group = models.CharField(max_length=250 , blank=False)
 
     def __str__ (self):
         return '%s %s %s' % (self.orderNumber, self.customerDesc, self.group)
@@ -19,10 +19,10 @@ class displayBackground(models.Model):
     image = models.ImageField(upload_to='display/images/', blank=True)
 
 class custID(models.Model):
-    username = models.CharField(max_length=250)
-    customerDesc = models.CharField(max_length=250)
-    group = models.CharField(max_length=250)
-    level = models.CharField(max_length=250)
+    username = models.CharField(max_length=250, unique=True, blank=False)
+    customerDesc = models.CharField(max_length=250, blank=False)
+    group = models.CharField(max_length=250 ,blank=False)
+    level = models.CharField(max_length=250, blank=False)
     isAdmin = models.BooleanField(default=False)
 
 
